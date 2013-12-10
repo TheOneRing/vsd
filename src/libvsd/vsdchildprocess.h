74e36465adcd3148d344d0f77a82ed787409dcb3
Patrick von Reth <vonreth@kde.org>
2013-12-10 21:43:05 +0100

    fixed copying header

--- src/libvsd/vsdchildprocess.h
+++ src/libvsd/vsdchildprocess.h
@@ -15,7 +15,7 @@
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with SnarlNetworkBridge.  If not, see <http://www.gnu.org/licenses/>.
+    along with VSD.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef VSDCHILDPROCESS_H
